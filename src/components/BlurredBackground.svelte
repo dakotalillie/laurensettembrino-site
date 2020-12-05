b740aa7ed0d3d53acd41631083645049fe6aa97a
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-05 14:36:13 -0500

    Fix blurred background position

--- src/components/BlurredBackground.svelte
+++ src/components/BlurredBackground.svelte
@@ -1,6 +1,6 @@
 <style>
   div {
-    @apply h-screen absolute left-0 top-0 w-screen;
+    @apply h-screen fixed left-0 top-0 w-screen;
     background: transparent url("/img/home-bg.jpg") no-repeat 25% fixed;
     background-size: cover;
     filter: blur(12px);
