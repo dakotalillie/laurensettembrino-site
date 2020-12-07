72b1f94f55dc439933bdd5dfdd99cdc4ddefbb5c
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-06 20:33:44 -0500

    Add mobile hamburger menu

--- src/components/Hamburger.svelte
+++ src/components/Hamburger.svelte
@@ -1,7 +1,4 @@
-<svg
-  class="fill-current text-white w-8 ml-4 lg:hidden md:w-10 "
-  viewBox="0 0 512 512"
-  xmlns="http://www.w3.org/2000/svg">
+<svg class="fill-current text-white w-8 lg:hidden md:w-10" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
   <path d="M441.13,166.52h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" />
   <path d="M441.13,279.72h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" />
   <path d="M441.13,392.92h-372a15,15,0,1,1,0-30h372a15,15,0,0,1,0,30Z" />
--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { slide } from "svelte/transition";
   import Hamburger from "./Hamburger.svelte";
 
   export let segment: string;
@@ -10,11 +12,39 @@
     { href: "media", label: "Media", segment: "media" },
     { href: "contact", label: "Contact", segment: "contact" },
   ];
+
+  let showMobileNav = false;
+  let hamburgerButton: HTMLButtonElement;
+  const toggleNav = () => (showMobileNav = !showMobileNav);
+
+  onMount(() => {
+    const listener = (event: MouseEvent) => {
+      if (!hamburgerButton.contains(event.target as Node)) {
+        showMobileNav = false;
+      }
+    };
+    document.addEventListener("click", listener, false);
+    return () => removeEventListener("click", listener);
+  });
 </script>
 
 <style>
   a[aria-current="page"] {
     @apply border-b-2 border-red-500;
+  }
+
+  .mobile-nav {
+    @apply absolute bg-black py-2 text-center right-0;
+  }
+
+  .mobile-nav li {
+    @apply py-2 px-4;
+  }
+
+  @screen lg {
+    .mobile-nav {
+      @apply hidden;
+    }
   }
 </style>
 
@@ -34,4 +64,17 @@
     <!-- <li><a rel="prefetch" aria-current={segment === 'blog' ? 'page' : undefined} href="blog">blog</a></li> -->
   </ul>
 </nav>
-<Hamburger />
+<div class="relative">
+  <button class="ml-4" bind:this={hamburgerButton} on:click={toggleNav}>
+    <Hamburger />
+  </button>
+  {#if showMobileNav}
+    <ul class="mobile-nav" in:slide={{ duration: 250 }}>
+      {#each links as link}
+        <li>
+          <a class="p-1" aria-current={segment === link.segment ? 'page' : undefined} href={link.href}>{link.label}</a>
+        </li>
+      {/each}
+    </ul>
+  {/if}
+</div>
