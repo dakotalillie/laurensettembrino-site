ef9dfe9996d4fb0308b78d8590eec28d4dafb116
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-08 14:03:08 -0500

    Webp background image

--- src/components/Background.svelte
+++ src/components/Background.svelte
@@ -12,6 +12,10 @@
     z-index: -1;
   }
 
+  :global(.webp) div {
+    background-image: url("/img/home-bg-mobile.webp");
+  }
+
   div.blurred {
     filter: blur(12px);
   }
@@ -20,19 +24,28 @@
     div {
       background-image: url("/img/home-bg-tablet.jpg");
     }
+
+    :global(.webp) div {
+      background-image: url("/img/home-bg-tablet.webp");
+    }
   }
 
   @screen lg {
     div {
       background-image: url("/img/home-bg-laptop.jpg");
     }
+
+    :global(.webp) div {
+      background-image: url("/img/home-bg-laptop.webp");
+    }
   }
 
   @screen xl {
-    div {
+    div,
+    :global(.webp) div {
       background-image: url("/img/home-bg.jpg");
     }
   }
 </style>
 
-<div class={isBlurred ? 'blurred' : ''} />
+<div class:blurred={isBlurred} />
