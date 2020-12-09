80afad6fc076ca88855604582fdaabd0083000f3
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-08 21:36:04 -0500

    Make form feedback text darker

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -75,9 +75,9 @@
         </label>
         <div class="flex flex-row justify-end space-x-4">
           {#if requestStatus === 'succeeded'}
-            <p class="flex items-center text-green-500">Your message has been sent!</p>
+            <p class="flex items-center text-green-600">Your message has been sent!</p>
           {:else if requestStatus === 'failed'}
-            <p class="flex items-center text-red-500">Oops! Something went wrong.</p>
+            <p class="flex items-center text-red-600">Oops! Something went wrong.</p>
           {/if}
           <button
             class="py-2 px-4 bg-blue-500 hover:bg-blue-400 text-white"
