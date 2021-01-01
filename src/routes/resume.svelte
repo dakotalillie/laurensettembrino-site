0d6c148e7c60932a8861ffcf105de3795a7cfdfd
Dakota Lillie <dakota.lillie@icloud.com>
2021-01-01 14:39:50 -0500

    Resume mobile formatting

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -3,17 +3,22 @@
 </script>
 
 <style>
-  .resume-section {
-    @apply mt-8 flex flex-row justify-between;
-  }
-
   .subheader {
-    @apply flex flex-row space-x-4 mt-4;
+    @apply flex flex-col space-y-2 mt-6 w-full;
   }
 
-  .subheader > span:not(:first-of-type):before {
-    @apply mr-4 text-red-700;
+  .subheader > span:before {
+    @apply mr-2 text-red-700;
     content: "\2666";
+  }
+
+  .section-grid {
+    @apply grid mt-8;
+    grid-template-columns: 1fr;
+  }
+
+  .section-grid > * {
+    grid-column: 1;
   }
 
   .content a {
@@ -28,13 +33,52 @@
     @apply mt-8 border-red-700;
   }
 
-  ul {
-    @apply pl-8;
-  }
-
   li:before {
     @apply mr-2 text-red-700;
     content: "\2666";
+  }
+
+  @screen sm {
+    .subheader {
+      @apply flex-row space-y-0 space-x-4 mt-4 justify-center;
+    }
+
+    .subheader > span:not(:first-of-type):before {
+      @apply mr-4;
+    }
+
+    .subheader > span:first-of-type:before {
+      @apply mr-0;
+      content: "";
+    }
+  }
+
+  @screen md {
+    .section-grid {
+      grid-template-columns: 1fr auto;
+    }
+
+    .section-grid .location {
+      grid-row: 1;
+      grid-column: 2;
+      text-align: right;
+    }
+
+    .balasole .location1 {
+      grid-row: 2;
+      grid-column: 2;
+      text-align: right;
+    }
+
+    .balasole .location2 {
+      grid-row: 4;
+      grid-column: 2;
+      text-align: right;
+    }
+
+    ul {
+      @apply pl-8;
+    }
   }
 </style>
 
@@ -59,131 +103,103 @@
   </div>
   <hr />
   <h3>Professional Experience</h3>
-  <div class="resume-section">
-    <div>
-      <p><strong>America-Israel Cultural Foundation’s ICA Awards</strong> (2019)</p>
-      <p>Ori Flomin's <em>Together, together</em></p>
-    </div>
-    <div>Rose Theater, Jazz at Lincoln Center</div>
+  <div class="section-grid">
+    <p><strong>America-Israel Cultural Foundation’s ICA Awards</strong> (2019)</p>
+    <p class="location">Rose Theater, Jazz at Lincoln Center</p>
+    <p>Ori Flomin's <em>Together, together</em></p>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Angie Moon Dance Theatre</strong></p>
-      <p><em>Company dancer since August 2018</em></p>
-      <ul>
-        <li>Angie Conte's <em>HUNTING YEAR, The Drowning Pine</em></li>
-      </ul>
-    </div>
-    <div>Venues throughout NYC</div>
+  <div class="section-grid">
+    <p><strong>Angie Moon Dance Theatre</strong></p>
+    <p><em>Company dancer since August 2018</em></p>
+    <p class="location">Venues throughout NYC</p>
+    <ul>
+      <li>Angie Conte's <em>HUNTING YEAR, The Drowning Pine</em></li>
+    </ul>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Neville Dance Theatre</strong></p>
-      <p><em>Company dancer since Fall 2017</em></p>
-      <ul>
-        <li>Brenda Neville's <em>Exposed, Elements (pointe), 53 Movements</em></li>
-      </ul>
-    </div>
-    <div>Milwaukee, WI; Venues throughout NYC</div>
+  <div class="section-grid">
+    <p><strong>Neville Dance Theatre</strong></p>
+    <p><em>Company dancer since Fall 2017</em></p>
+    <div class="location">Milwaukee, WI; Venues throughout NYC</div>
+    <ul>
+      <li>Brenda Neville's <em>Exposed, Elements (pointe), 53 Movements</em></li>
+    </ul>
   </div>
-  <div class="resume-section">
-    <div class="w-full">
-      <p><strong>BalaSole Dance Company</strong></p>
-      <p class="flex flex-row justify-between w-full">
-        <span><em>Featured Emerging Artist</em> (2017)</span><span>Brooklyn Academy of Music (Fisher), NYC</span>
-      </p>
-      <p class="flex flex-row justify-between w-full">
-        <span><em>Featured Soloist</em> (2018)</span><span>Ailey Citigroup Theater, NYC</span>
-      </p>
-      <ul>
-        <li>Original choreography: <em>B-Y</em></li>
-      </ul>
-    </div>
+  <div class="balasole section-grid">
+    <p><strong>BalaSole Dance Company</strong></p>
+    <p><em>Featured Soloist</em> (2018)</p>
+    <p class="location1">Ailey Citigroup Theater, NYC</p>
+    <ul>
+      <li>Original choreography: <em>B-Y</em></li>
+    </ul>
+    <p class="mt-2 md:mt-0"><em>Featured Emerging Artist</em> (2017)</p>
+    <p class="location2">Brooklyn Academy of Music (Fisher), NYC</p>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Second Avenue Dance Company</strong> (2016-2017)</p>
-      <p>Stefanie Batten Bland's <em>Incoming</em></p>
-    </div>
-    <div>Jack Crystal Theater, NYC</div>
+  <div class="section-grid">
+    <p><strong>Second Avenue Dance Company</strong> (2016-2017)</p>
+    <p class="location">Jack Crystal Theater, NYC</p>
+    <p>Stefanie Batten Bland's <em>Incoming</em></p>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Lincoln Center White Light Festival</strong> (2015)</p>
-      <p>Crystal Pite's <em>Polaris</em></p>
-      <ul>
-        <li>One of 60 Tisch students chosen to perform with Kidd Pivot</li>
-      </ul>
-    </div>
-    <div>New York City Center, NYC</div>
+  <div class="section-grid">
+    <p><strong>Lincoln Center White Light Festival</strong> (2015)</p>
+    <p class="location">New York City Center, NYC</p>
+    <p>Crystal Pite's <em>Polaris</em></p>
+    <ul>
+      <li>One of 60 Tisch students chosen to perform with Kidd Pivot</li>
+    </ul>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>The Nutcracker NYC</strong> (2015, 2016)</p>
-      <p>Choreography and direction by Giada Ferrone</p>
-      <ul>
-        <li>Soloist and ensemble roles</li>
-      </ul>
-    </div>
-    <div>Ailey Citigroup Theater, NYC</div>
+  <div class="section-grid">
+    <p><strong>The Nutcracker NYC</strong> (2015, 2016)</p>
+    <p class="location">Ailey Citigroup Theater, NYC</p>
+    <p>Choreography and direction by Giada Ferrone</p>
+    <ul>
+      <li>Soloist and ensemble roles</li>
+    </ul>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Tisch School of the Arts, New York University</strong> (2014 - 2017)</p>
-      <p>Elizabeth Coker's <em>From Then To</em></p>
-      <p>Numerous contemporary and contemporary ballet student works</p>
-    </div>
-    <div>Jack Crystal Theater, NYC</div>
+  <div class="section-grid">
+    <p><strong>Tisch School of the Arts, New York University</strong> (2014-2017)</p>
+    <p class="location">Jack Crystal Theater, NYC</p>
+    <p>Elizabeth Coker's <em>From Then To</em></p>
+    <p>Numerous contemporary and contemporary ballet student works</p>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Dizzy Feet Foundation - Celebration of Dance Gala</strong> (2015)</p>
-      <p>Erin McNerney's <em>Feed the Birds</em></p>
-    </div>
-    <div>Los Angeles, CA</div>
+  <div class="section-grid">
+    <p><strong>Dizzy Feet Foundation - Celebration of Dance Gala</strong> (2015)</p>
+    <p class="location">Los Angeles, CA</p>
+    <p>Erin McNerney's <em>Feed the Birds</em></p>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Classical Repertory Experience:</strong></p>
-      <p>Variations from <em>Paquita, Don Quixote, La Fille Mal Gardee, La Bayadere</em></p>
-      <p>Grand pas from <em>Paquita</em> and <em>The Nutcracker</em></p>
-    </div>
+  <div class="section-grid">
+    <p><strong>Classical Repertory Experience:</strong></p>
+    <p>Variations from <em>Paquita, Don Quixote, La Fille Mal Gardee, La Bayadere</em></p>
+    <p>Grand pas from <em>Paquita</em> and <em>The Nutcracker</em></p>
   </div>
   <hr />
   <h3>Training/Education</h3>
-  <div class="resume-section">
-    <div>
-      <p><strong>Tisch School of the Arts, New York University</strong></p>
-      <p><em>BFA in Dance, May 2017</em></p>
-      <ul>
-        <li><em>Contemporary</em>: Rashaun Mitchell, Pamela Pietro, Elizabeth Coker, Ori Flomin</li>
-        <li><em>Ballet</em>: Giada Ferrone, Cherylyn Lavagnino, James Martin, Jolinda Menendez (pointe)</li>
-        <li><em>Composition and improvisation</em>: Rashaun Mitchell, Jermey Nelson, Sean Curran</li>
-      </ul>
-    </div>
-    <div>New York, NY</div>
+  <div class="section-grid">
+    <p><strong>Tisch School of the Arts, New York University</strong></p>
+    <p class="location">New York, NY</p>
+    <p><em>BFA in Dance, May 2017</em></p>
+    <ul>
+      <li><em>Contemporary</em>: Rashaun Mitchell, Pamela Pietro, Elizabeth Coker, Ori Flomin</li>
+      <li><em>Ballet</em>: Giada Ferrone, Cherylyn Lavagnino, James Martin, Jolinda Menendez (pointe)</li>
+      <li><em>Composition and improvisation</em>: Rashaun Mitchell, Jermey Nelson, Sean Curran</li>
+    </ul>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>College of Arts and Sciences, New York University</strong></p>
-      <p><em>Double major in English</em> (2017)</p>
-      <ul>
-        <li>Cum Laude, University Honors Scholar</li>
-      </ul>
-    </div>
-    <div>New York,NY</div>
+  <div class="section-grid">
+    <p><strong>College of Arts and Sciences, New York University</strong></p>
+    <p class="location">New York,NY</p>
+    <p><em>Double major in English</em> (2017)</p>
+    <ul>
+      <li>Cum Laude, University Honors Scholar</li>
+    </ul>
   </div>
-  <div class="resume-section">
-    <div>
-      <p><strong>Domaine Forget Summer Dance Academy</strong> (2016)</p>
-      <p><em>RUBBERBANDance Group's summer residency program</em></p>
-      <ul>
-        <li>Trained with Victor Quijada, Anne Plamondon, and members of RUBBERBANDance Group</li>
-      </ul>
-    </div>
-    <div>Quebec, CAN</div>
+  <div class="section-grid">
+    <p><strong>Domaine Forget Summer Dance Academy</strong> (2016)</p>
+    <p class="location">Quebec, CAN</p>
+    <p><em>RUBBERBANDance Group's summer residency program</em></p>
+    <ul>
+      <li>Trained with Victor Quijada, Anne Plamondon, and members of RUBBERBANDance Group</li>
+    </ul>
   </div>
-  <div class="resume-section">
+  <div class="section-grid">
     <p>
       <strong>Other dance skills:</strong>
       pointe, improvisation, contemporary and ballet partnering, choreography (concert and commercial), tap, music
@@ -191,7 +207,7 @@
     </p>
   </div>
   <hr />
-  <div class="resume-section">
+  <div class="section-grid">
     <p>
       <strong>Other skills:</strong>
       acting, singing, grant writing, editing/proofreading, search engine optimization (SEO)
