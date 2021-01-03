2a3879c7e46e7881ed819395b333dea601dd370a
Dakota Lillie <dakota.lillie@icloud.com>
2021-01-03 11:11:01 -0500

    Lazy load images on photos page

--- src/components/Photos.svelte
+++ src/components/Photos.svelte
@@ -1,135 +1,161 @@
 <script lang="ts">
-  import { onMount, onDestroy, tick } from "svelte";
+  import { onMount, tick } from "svelte";
   import Modal from "./Modal.svelte";
   import debounce from "../utils/debounce";
 
   import "@splidejs/splide/dist/css/themes/splide-default.min.css";
+  import IntersectionObserver from "./IntersectionObserver.svelte";
 
   const pictures = [
     {
       id: "18",
       alt: "Lauren Settembrino in green shirt hinges backward toward floor",
       caption: "Photo Credit: Eric Bandiero / <em>B–Y</em> by Lauren Settembrino with BalaSole Dance Company",
+      height: 400,
     },
     {
       id: "2",
       alt: "Lauren Settembrino mid-jump in gray jumpsuit",
       caption: "Photo Credit: Benny Krown / <em>The Drowning Pine</em>, Angie Moon Dance Theatre",
+      height: 750,
     },
     {
       id: "21",
       alt: "Lauren Settembrino, in pink, white, and gray leotard, performs layout with leg in attitude front",
       caption: "Photo Credit: Alex Tenreiro Theis",
+      height: 317,
     },
     {
       id: "1",
       alt: "Lauren Settembrino with leg held side against pink background",
       caption: "Photo Credit: Ella Bromblin / <em>Together, together</em> by Ori Flomin",
+      height: 419,
     },
     {
       id: "9",
       alt: "Lauren Settembrino holds open hand near face with leg raised behind her",
       caption: "Photo Credit: Meghann Padgett / Angie Moon Dance Theatre rehearsal",
+      height: 333,
     },
     {
       id: "14",
       alt: "Lauren Settembrino and partner hold right leg up in front of her",
       caption: "Photo Credit: Benny Krown / <em>The Drowning Pine</em>, Angie Moon Dance Theatre",
+      height: 333,
     },
     {
       id: "4",
       alt: "Lauren Settembrino in orange leotard held by partner in arabesque press lift",
       caption: "Neville Dance Theatre rehearsal",
+      height: 541,
     },
     {
       id: "17",
       alt: "Lauren Settembrino in green shirt tosses right arm up and head back with legs wide",
       caption: "Photo Credit: Eric Bandiero / <em>B–Y</em> by Lauren Settembrino with BalaSole Dance Company",
+      height: 400,
     },
     {
       id: "3",
       alt: "Lauren Settembrino with leg in passe surrounded by large plastic sheets",
       caption: "Photo Credit: Ella Bromblin / <em>Layers</em> by Janae Bonnen, Angie Lu, Evita Zacharioglou",
+      height: 501,
     },
     {
       id: "11",
       alt: "Lauren Settembrino reaching arms vertically in opposite directions against blue background",
       caption: "Photo Credit: Andrea Pugliese Trager / <em>Wear and Tear</em> by Hayoung Roh",
+      height: 333,
     },
     {
       id: "15",
       alt: "Spotlight on Lauren Settembrino in arabesque holding on to group of dancers",
       caption: "Photo Credit: Benny Krown / <em>The Drowning Pine</em>, Angie Moon Dance Theatre",
+      height: 333,
     },
     {
       id: "8",
       alt: "Profile of Lauren Settembrino curling fingers near mouth while dancing",
       caption: "Photo Credit: Meghann Padgett / Angie Moon Dance Theatre rehearsal",
+      height: 333,
     },
     {
       id: "24",
       alt: "Lauren Settembrino, on pointe, hinges away from hands pushing down",
       caption: "Neville Dance Theatre",
+      height: 715,
     },
     {
       id: "20",
       alt: "Lauren Settembrino, in green shirt and black pants, kicks leg to head",
       caption: "Photo Credit: Eric Bandiero / <em>B–Y</em> by Lauren Settembrino with BalaSole Dance Company",
+      height: 400,
     },
     {
       id: "12",
       alt: "Lauren Settembrino holds right leg in high side attitude in rehearsal studio",
       caption: "Photo Credit: Alex Tenreiro Theis",
+      height: 333,
     },
     {
       id: "16",
       alt: "Lauren Settembrino in front of group of dancers dressed in white holding releve",
       caption: "Photo Credit: Haleigh Dalke / <em>I Am ___</em> by Chad Vaught",
+      height: 333,
     },
     {
       id: "7",
       alt: "Lauren Settembrino in peach skirt arches back during outdoor performance",
       caption: "Photo Credit: Ori Highbloom / <em>Together, together</em> by Ori Flomin",
+      height: 500, // not the actual height, but what it would be if the width of the image were 500 like it is for all the others
     },
     {
       id: "10",
       alt: "Black and white photo of Lauren Settembrino looking away with one arm extended",
       caption: "Photo Credit: Meghann Padgett / Angie Moon Dance Theatre rehearsal",
+      height: 333,
     },
     {
       id: "25",
       alt: "Lauren Settembrino in spotlight arches back to show neck and collarbone",
       caption: "Photo Credit: Benny Krown / <em>The Drowning Pine</em>, Angie Moon Dance Theatre",
+      height: 333,
     },
     {
       id: "22",
       alt: "Lauren Settembrino in green shirt and black pants performs on stage",
       caption: "Photo Credit: Eric Bandiero / <em>B–Y</em> by Lauren Settembrino with BalaSole Dance Company",
+      height: 400,
     },
     {
       id: "6",
       alt: "Lauren Settembrino holding high arabesque against blue background",
       caption: "Photo Credit: Ella Bromblin / <em>Snowflakes, The Nutcracker NYC</em> by Giada Matteini",
+      height: 498,
     },
     {
       id: "5",
       alt: "Lauren Settembrino kicks while rehearsing with male dancer in studio",
       caption: "Neville Dance Theatre rehearsal",
+      height: 403,
     },
     {
       id: "19",
       alt: "Lauren Settembrino, with relaxed expression, extends right leg with flexed foot",
       caption: "Photo Credit: Eric Bandiero / <em>B–Y</em> by Lauren Settembrino with BalaSole Dance Company",
+      height: 400,
     },
     {
       id: "23",
       alt: "Lauren Settembrino in burgundy leotard extends leg to the side while on pointe",
       caption: "Neville Dance Theatre",
+      height: 716,
     },
     {
       id: "13",
       alt: "Lauren Settembrino squats low with legs crossed in gray, white, and pink leotard",
       caption: "Photo Credit: Alex Tenreiro Theis",
+      height: 347,
     },
   ];
 
@@ -139,8 +165,8 @@
   let section: HTMLElement;
   let gap: number;
   let items: HTMLElement[];
-  let loadedCount = 0;
   let modalContentVisible = false;
+  let measured = false;
   let carousel;
   let Splide;
   let splideInstance;
@@ -152,7 +178,10 @@
     const width = (section.clientWidth - allGapsWidth) / numberOfColumns;
 
     section.style.gridTemplateColumns = `repeat(${numberOfColumns}, ${width}px)`;
-    items.forEach((c) => c.style.removeProperty("margin-top"));
+    items.forEach((c) => {
+      c.style.removeProperty("margin-top");
+      c.style.height = `${+c.dataset.height * (width / MAX_COLUMN_WIDTH)}px`;
+    });
 
     if (numberOfColumns > 1) {
       items.slice(numberOfColumns).forEach((col, i) => {
@@ -162,7 +191,7 @@
       });
     }
 
-    items.forEach((c) => c.removeAttribute("data-measuring"));
+    measured = true;
   }, 250);
 
   onMount(() => {
@@ -170,17 +199,10 @@
     items = Array.from(section.childNodes).filter((c): c is HTMLElement => {
       return c.nodeType === ELEMENT_NODE_TYPE && (c as HTMLElement).tagName === "BUTTON";
     });
+    layout();
+    addEventListener("resize", layout, false);
+    return () => removeEventListener("resize", layout);
   });
-
-  onDestroy(() => {
-    if (typeof removeEventListener !== "undefined") {
-      removeEventListener("resize", layout);
-    }
-  });
-
-  function handleLoad() {
-    loadedCount++;
-  }
 
   function handleLoadFull() {
     modalContentVisible = true;
@@ -226,11 +248,6 @@
     modalContentVisible = false;
     removeEventListener("resize", handleCarouselResize);
   }
-
-  $: if (loadedCount && items?.length && loadedCount === items?.length) {
-    layout();
-    addEventListener("resize", layout, false);
-  }
 </script>
 
 <style>
@@ -255,12 +272,6 @@
 
   button {
     align-self: start;
-    opacity: 1;
-    transition: opacity 500ms;
-  }
-
-  button[data-measuring] {
-    opacity: 0;
   }
 
   :global(.splide__spinner) {
@@ -281,18 +292,27 @@
 </style>
 
 <section bind:this={section}>
-  {#each pictures as { id, alt }, index}
+  {#each pictures as { id, alt, height }, index}
     <Modal
       className={modalContentVisible ? 'opacity-100' : 'opacity-0'}
       onClose={handleClose}
       onOpen={handleOpen}
       let:open>
-      <button slot="trigger" data-measuring="true" on:click={() => handleClick(open, index)}>
-        <picture>
-          <source srcset={`img/${id}.webp`} type="image/webp" />
-          <source srcset={`img/${id}.jpg`} type="image/jpeg" />
-          <img class="thumbnail" src={`img/${id}.jpg`} {alt} on:load={handleLoad} />
-        </picture>
+      <button
+        slot="trigger"
+        data-height={height}
+        style={`height: ${height}px`}
+        data-measuring="true"
+        on:click={() => handleClick(open, index)}>
+        <IntersectionObserver top={80} once let:intersecting>
+          {#if measured && intersecting}
+            <picture>
+              <source srcset={`img/${id}.webp`} type="image/webp" />
+              <source srcset={`img/${id}.jpg`} type="image/jpeg" />
+              <img class="thumbnail" src={`img/${id}.jpg`} {alt} />
+            </picture>
+          {/if}
+        </IntersectionObserver>
       </button>
       <div bind:this={carousel} class="relative" slot="content">
         <div class="splide__track">
