140e34130835a3cd7cd2ef3e233cda79ef254095
Dakota Lillie <dakota.lillie@icloud.com>
2022-01-29 04:13:52 -0500

    build: use static adapter for prod builds

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapterAuto from "@sveltejs/adapter-auto";
+import adapterStatic from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,7 @@ const config = {
   preprocess: preprocess({ postcss: true }),
 
   kit: {
-    adapter: adapter(),
+    adapter: process.env.NODE_ENV === "production" ? adapterStatic() : adapterAuto(),
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
