f9675de12d7c24ea8cb6fe41b4df65921e4b4446
Dakota Lillie <dakota.lillie@icloud.com>
2025-01-04 10:22:23 -0800

    Fix request status bug

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -28,8 +28,8 @@
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, email, subject, message }),
     })
-      .then(() => {
-        requestStatus = "succeeded";
+      .then((req) => {
+        requestStatus = req.ok ? "succeeded" : "failed";
       })
       .catch(() => {
         requestStatus = "failed";
